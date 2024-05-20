f7aea9799f7796e60eb92281c0f9afaf5474ac53
Gabriel Campolina <gcampolinap@gmail.com>
2024-05-19 23:39:39 -0300

    add section of projects

--- script.js
+++ script.js
@@ -17,5 +17,26 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHeight = document.querySelector('.navbar-box').offsetHeight;
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - navbarHeight;
+
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
 
 
